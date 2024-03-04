lynxis/kamailio

commit a627c9a04a74b48760441be3411f75325f1cb2f2

ims_registrar_scscf: Extend save() with optional flag
Add a new flag as optional parameter for save() method.
The flag contains one value: DO_NOT_USE_REALM_FOR_PRIVATE_IDENTITY
0x01. No changes save() when parameter is not present.

src/modules/ims_registrar_scscf/ims_registrar_scscf_mod.c
@@ -133,6 +133,8 @@ static int w_lookup_path_to_contact(struct sip_msg* _m, char* contact_uri);
 /*! \brief Fixup functions */
 static int domain_fixup(void** param, int param_no);
 static int assign_save_fixup3_async(void** param, int param_no);
+static int free_uint_fixup(void** param, int param_no);
+static int save_fixup3(void** param, int param_no);
 static int unreg_fixup(void** param, int param_no);
 static int fetchc_fixup(void** param, int param_no);
 /*! \brief Functions */
@@ -215,6 +217,8 @@ static pv_export_t mod_pvs[] = {
  */
 static cmd_export_t cmds[] = {
     {"save", (cmd_function) w_save, 2, assign_save_fixup3_async, 0, REQUEST_ROUTE | ONREPLY_ROUTE},
+    {"save", (cmd_function) w_save, 3, assign_save_fixup3_async, 0, REQUEST_ROUTE | ONREPLY_ROUTE},
+    {"save", (cmd_function) w_save, 4, save_fixup3, free_uint_fixup, REQUEST_ROUTE | ONREPLY_ROUTE},
     {"lookup", (cmd_function) w_lookup, 1, domain_fixup, 0, REQUEST_ROUTE | FAILURE_ROUTE},
     {"lookup", (cmd_function) w_lookup_ue_type, 2, domain_fixup, 0, REQUEST_ROUTE | FAILURE_ROUTE},
     {"lookup_path_to_contact", (cmd_function) w_lookup_path_to_contact, 1, fixup_var_str_12, 0, REQUEST_ROUTE},
@@ -620,7 +624,11 @@ AAAMessage* callback_cdp_request(AAAMessage *request, void *param) {
  * Wrapper to save(location)
  */
 static int w_save(struct sip_msg* _m, char* _route, char* _d, char* mode, char* _cflags) {
-    return save(_m, _d, _route);
+    if(_cflags){
+        return save(_m, _d, _route, ((int)(*_cflags)));
+    }
+
+    return save(_m, _d, _route, 0);
 }
 
 static int w_assign_server_unreg(struct sip_msg* _m, char* _route, char* _d, char* _direction) {
@@ -696,6 +704,68 @@ static int assign_save_fixup3_async(void** param, int param_no) {
             return -1;
         }
         *param = (void*) d;
+    }
+
+    return 0;
+}
+
+static int unit_fixup(void** param, int param_no)
+{
+	str s;
+	unsigned int* num;
+
+	if(*param){
+		num = (unsigned int*)pkg_malloc(sizeof(unsigned int));
+		*num = 0;
+
+		s.s = *param;
+		s.len = strlen(s.s);
+
+		if (likely(str2int(&s, num) == 0)) {
+			*param = (void*)(long)num;
+		}else{
+			LM_ERR("failed to convert to int\n");
+			pkg_free(num);
+			return E_UNSPEC;
+		}
+	}else{
+		return E_UNSPEC;
+	}
+
+	return 0;
+}
+
+static int free_uint_fixup(void** param, int param_no)
+{
+	if(*param && param_no == 2){
+		pkg_free(*param);
+		*param = 0;
+	}
+	return 0;
+}
+
+static int save_fixup3(void** param, int param_no) {
+    if (strlen((char*) *param) <= 0) {
+        LM_ERR("empty parameter %d not allowed\n", param_no);
+        return -1;
+    }
+
+    if (param_no == 1) {        //route name - static or dynamic string (config vars)
+        if (fixup_spve_null(param, param_no) < 0)
+            return -1;
+        return 0;
+    } else if (param_no == 2) {
+        udomain_t* d;
+
+        if (ul.register_udomain((char*) *param, &d) < 0) {
+            LM_ERR("Error doing fixup on save");
+            return -1;
+        }
+        *param = (void*) d;
+    } else if (param_no == 3) {
+        return 0;
+    } else if (param_no == 4) {
+        return unit_fixup(param, param_no);
     }
 
     return 0;

src/modules/ims_registrar_scscf/doc/ims_registrar_scscf_admin.xml
@@ -438,7 +438,7 @@
 
     <section>
       <title><function moreinfo="none">save(async_reply_route,
-      domain)</function></title>
+      domain, mode, flags)</function></title>
 
       <para>The function processes a REGISTER message. It can add, remove or
       modify usrloc records depending on Contact and Expires HFs in the
@@ -461,6 +461,16 @@
         <listitem>
           <para><emphasis>domain</emphasis>- Logical domain within
           registrar.</para>
+        </listitem>
+
+        <listitem>
+          <para><emphasis>mode</emphasis>- Optional and unused legacy parameter.</para>
+        </listitem>
+
+        <listitem>
+          <para><emphasis>flags</emphasis>- Optional parameter. Valid values:
+          0x01 - Realm is not used when extracting Private Identity from the
+          Authorization header.</para>
         </listitem>
       </itemizedlist>
 

src/modules/ims_registrar_scscf/save.c
@@ -84,6 +84,8 @@ extern int store_data_on_dereg; /**< should we store SAR user data on de-registr
 extern int ue_unsubscribe_on_dereg;
 extern int user_data_always;
 
+#define DO_NOT_USE_REALM_FOR_PRIVATE_IDENTITY         0x01
+
 /* \brief
  * Return randomized expires between expires-range% and expires.
  * RFC allows only value less or equal to the one provided by UAC.
@@ -1276,13 +1278,13 @@ error:
  */
 //int save(struct sip_msg* msg, udomain_t* _d) {
 
-int save(struct sip_msg* msg, char* str1, char *route) {
+int save(struct sip_msg* msg, char* str1, char *route, int _cflags) {
     int expires;
     int require_user_data = 0;
     int data_available;
     contact_t* c;
     int st;
-    str public_identity, private_identity, realm;
+    str public_identity, private_identity, realm={0,0};
     int sar_assignment_type = AVP_IMS_SAR_NO_ASSIGNMENT;
     str route_name;
 
@@ -1342,12 +1344,15 @@ int save(struct sip_msg* msg, char* str1, char *route) {
         rerrno = R_SAR_FAILED;
         goto error;
     }
-    realm = cscf_get_realm_from_uri(public_identity);
-    if (realm.len <= 0 || !realm.s) {
-        LM_ERR("can't get realm\n");
-        rerrno = R_SAR_FAILED;
-        goto error;
-    }
+
+    if (!(_cflags & DO_NOT_USE_REALM_FOR_PRIVATE_IDENTITY)) {
+        realm = cscf_get_realm_from_uri(public_identity);
+        if (realm.len <= 0 || !realm.s) {
+            LM_ERR("can't get realm\n");
+            rerrno = R_SAR_FAILED;
+            goto error;
+        }
+     }
 
     private_identity = cscf_get_private_identity(msg, realm);
     if (private_identity.len <= 0 || !private_identity.s) {

src/modules/ims_registrar_scscf/save.h
@@ -58,7 +58,7 @@
  */
 int assign_server_unreg(struct sip_msg* _m, char* str1, str* direction, char* route);
 
-int save(struct sip_msg* msg, char* str1, char* route);
+int save(struct sip_msg* msg, char* str1, char* route, int _cflags);
 
 int unregister(struct sip_msg* _m, char* _d, char* _uri);
 

commit ebcd17699a1c72a3f329c8764a19765cddc61d92

ims_registrar_scscf: fix uninitialized arguments in save()
The script "save" command can be used with different arguments:
2, 3 and 4.
But internally for all save() calls the exact same function is use, w_save().
When calling save("PRE_REG_SAR_REPLY","location"); as given by the examples,
kamailio will call w_save().
Because the command code will cast the function pointer, the w_save() function
is called with 2 uninitialized arguments *mode, *c_flags.
mode is unused, seems a legacy argument. c_flags is referenced resulting in a crash
if not null.

Use separate wrapper functions to handle different argument number.

Fixes: a627c9a04a74 ("ims_registrar_scscf: Extend save() with optional flag")

src/modules/ims_registrar_scscf/ims_registrar_scscf_mod.c
@@ -131,7 +131,11 @@ struct _pv_req_data _pv_treq;
 static int mod_init(void);
 static int child_init(int);
 static void mod_destroy(void);
-static int w_save(
+static int w_save2(
+    struct sip_msg *_m, char *_route, char *_d);
+static int w_save3(
+    struct sip_msg *_m, char *_route, char *_d, char *mode);
+static int w_save4(
 		struct sip_msg *_m, char *_route, char *_d, char *mode, char *_cflags);
 static int w_assign_server_unreg(
 		struct sip_msg *_m, char *_route, char *_d, char *_direction);
@@ -231,11 +235,11 @@ static pv_export_t mod_pvs[] = {
  * Exported functions
  */
 static cmd_export_t cmds[] = {
-		{"save", (cmd_function)w_save, 2, assign_save_fixup3_async, 0,
+		{"save", (cmd_function)w_save2, 2, assign_save_fixup3_async, 0,
 				REQUEST_ROUTE | ONREPLY_ROUTE},
-		{"save", (cmd_function)w_save, 3, assign_save_fixup3_async, 0,
+		{"save", (cmd_function)w_save3, 3, assign_save_fixup3_async, 0,
 				REQUEST_ROUTE | ONREPLY_ROUTE},
-		{"save", (cmd_function)w_save, 4, save_fixup3, free_uint_fixup,
+		{"save", (cmd_function)w_save4, 4, save_fixup3, free_uint_fixup,
 				REQUEST_ROUTE | ONREPLY_ROUTE},
 		{"lookup", (cmd_function)w_lookup, 1, domain_fixup, 0,
 				REQUEST_ROUTE | FAILURE_ROUTE},
@@ -677,9 +681,23 @@ AAAMessage *callback_cdp_request(AAAMessage *request, void *param)
 /*! \brief
  * Wrapper to save(location)
  */
-static int w_save(
+static int w_save2(
+    struct sip_msg *_m, char *_route, char *_d)
+{
+	return save(_m, _d, _route, 0);
+}
+
+static int w_save3(
+    struct sip_msg *_m, char *_route, char *_d, char *_mode)
+{
+	/* mode is unsed. Docs says legacy parameter? Maybe to be compatible with registrar/save? */
+	return save(_m, _d, _route, 0);
+}
+
+static int w_save4(
 		struct sip_msg *_m, char *_route, char *_d, char *mode, char *_cflags)
 {
+	/* mode is unsed. Docs says legacy parameter? Maybe to be compatible with registrar/save? */
 	if(_cflags) {
 		return save(_m, _d, _route, ((int)(*_cflags)));
 	}